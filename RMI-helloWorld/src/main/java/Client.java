6cb55c352dcc3fdb7851cf0d15f370f04a171000
MichelKoolwaaij <michel.koolwaaij@han.nl>
2017-09-07 11:44:53 +0200

    RMI added

---
+++ RMI-helloWorld/src/main/java/Client.java
@@ -0,0 +1,19 @@
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class Client {
+    private Client() {}
+
+    public static void main(String[] args) {
+
+        try {
+            Registry registry = LocateRegistry.getRegistry();
+            Hello stub = (Hello) registry.lookup("Hello");
+            String response = stub.sayHello();
+            System.out.println("response: " + response);
+        } catch (Exception e) {
+            System.err.println("Client exception: " + e.toString());
+            e.printStackTrace();
+        }
+    }
+}
