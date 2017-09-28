840050b68d4ae2077d1767c77eb3416ebf7129c0
MichelKoolwaaij <michel.koolwaaij@han.nl>
2017-09-28 10:45:30 +0200

    Oefenen met patterns en refactoring

---
+++ SlechteCode origineel/src/main/java/mainApp.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Michel Koolwaaij on 29-09-16.
+ */
+public class mainApp {
+    public static void main(String[] args){
+
+        FruitPlukker plukker = new FruitPlukker("TOMAAT");
+        plukker.printFruit();
+    }
+}
